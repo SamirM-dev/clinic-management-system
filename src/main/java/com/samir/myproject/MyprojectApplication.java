ac53e2535ff4b0c28dab3351dba29072e02217f2
SamirM-dev <memmedovsamirws@gmail.com>
2025-10-23 17:52:34 +0400

    Initial Spring Boot project

---
+++ src/main/java/com/samir/myproject/MyprojectApplication.java
@@ -0,0 +1,15 @@
+package com.samir.myproject;
+
+import com.samir.myproject.Entities.Employee;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyprojectApplication {
+
+	public static void main(String[] args) {
+        SpringApplication.run(MyprojectApplication.class, args);
+	}
+
+}
